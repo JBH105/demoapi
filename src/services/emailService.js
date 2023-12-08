3996649e586e1f085d635b96acd8e6e2389b713b
jbh105 <jaydipbhayani105@gmail.com>
2023-12-08 19:08:17 +0530

    create send email api

---
+++ src/services/emailService.js
@@ -0,0 +1,46 @@
+const transporter = require("../config/email");
+
+const sendEmail = async (emailData) => {
+  try {
+    const { to, subject, text, attachment } = emailData;
+
+    if (!to) {
+      throw new Error('Missing "to" address');
+    }
+
+    if (!subject) {
+      throw new Error("Missing email subject");
+    }
+
+    if (!text) {
+      throw new Error("Missing email body text");
+    }
+
+    if (!attachment) {
+      throw new Error("Missing email attachment");
+    }
+
+    const mailOptions = {
+      from: "no-reply@devnetworks.org",
+      to,
+      subject,
+      html: text,
+      attachments: [
+        {
+          filename: "document.pdf",
+          content: attachment,
+          encoding: "binary",
+          ...(emailData.originalname && { filename: emailData.originalname }),
+        },
+      ],
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent:", info.messageId);
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw error;
+  }
+};
+
+module.exports = { sendEmail };
